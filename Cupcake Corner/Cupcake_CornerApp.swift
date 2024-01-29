0eb9249fe5b6ddcd76dfdde5077c6f5d3cd1272e
Mit Kamal Sheth <sheth.mit@northeastern.edu>
2024-01-28 21:15:25 -0500

    Initial Commit

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Mit Sheth on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
